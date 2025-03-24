0605d39dd6585392493cc5370a07c8cefa3e4820
Marcel Graber <marcel@clever.design>
2025-03-24 11:44:18 +0100

    feat: add common  module

--- platform/esp.cmake
+++ platform/esp.cmake
@@ -3,11 +3,13 @@ cmake_minimum_required(VERSION 3.20)
 # CHECK needed for esp-idf build system to avoid errors if(NOT
 # CMAKE_BUILD_EARLY_EXPANSION)
 if(NOT CMAKE_BUILD_EARLY_EXPANSION)
-  message(STATUS "Building tinylibs for ESP-IDF *******************")
+  message(
+    STATUS "Building tinylibs for ESP-IDF *******************${COMPONENT_LIB}")
   tiny_library_named(tinysettings)
   # Include tinyplatform
-  add_subdirectory(${TINYSETTINGS_DIR}/lib/tinyplatform
-                   ${TINYSETTINGS_DIR}/lib/tinyplatform)
+
+  # add_subdirectory(${TINYSETTINGS_DIR}/lib/tinyplatform
+  #                  ${TINYSETTINGS_DIR}/lib/tinyplatform)
 
   tiny_include_directories(${TINYSETTINGS_DIR}/include)
   add_subdirectory(${TINYSETTINGS_DIR}/src)
--- platform/zephyr.cmake
+++ platform/zephyr.cmake
@@ -4,7 +4,7 @@ tiny_library_named(tinysettings)
 
 # Include tinyplatform
 add_subdirectory(${TINYSETTINGS_DIR}/lib/tinyplatform
-                 ${TINYSETTINGS_DIR}/lib/tinyplatform)
+                 ${TINYSETTINGS_DIR}/build/tiny)
 
 tiny_include_directories(${TINYSETTINGS_DIR}/include)
 add_subdirectory(${TINYSETTINGS_DIR}/src)
