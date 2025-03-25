30c65c66748e73da694c34c3bf0824544b726bc4
Marcel Graber <marcel@clever.design>
2025-03-25 06:43:10 +0100

    fix: remove unused settings in Kconfig

--- platform/esp.cmake
+++ platform/esp.cmake
@@ -6,5 +6,3 @@ tiny_library_named(tinysettings)
 tiny_include_directories(${TINYSETTINGS_DIR}/include)
 add_subdirectory(${TINYSETTINGS_DIR}/src)
 target_link_libraries(${COMPONENT_LIB} INTERFACE tinysettings)
-
-message(STATUS "Building tinysettings *******************${COMPONENT_LIB}")
